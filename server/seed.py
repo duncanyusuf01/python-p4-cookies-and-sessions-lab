26d7b28533f9380a3cc6b8e61a27d8044b1f24d1
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-06-29 16:48:55 +0300

    finished lab

--- server/seed.py
+++ server/seed.py
@@ -7,6 +7,7 @@ from faker import Faker
 from app import app
 from models import db, Article, User
 
+
 fake = Faker()
 
 with app.app_context():
@@ -41,3 +42,4 @@ with app.app_context():
     
     db.session.commit()
     print("Complete.")
+    
